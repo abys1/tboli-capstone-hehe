f6cd829e335e1d16d09b18aa62b4076e7e981109
Bang <zndrsmbng@gmail.com>
2023-07-10 08:39:53 +0800

    title pages

--- admin_archive_lessons.php
+++ admin_archive_lessons.php
@@ -6,7 +6,7 @@
 <!DOCTYPE html>
 <html lang="en" class="menuitem-active"><head>
     <meta charset="utf-8">
-    <title>Starter Page | Hyper - Responsive Bootstrap 5 Admin Dashboard</title>
+    <title>Archive Lesson</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
     <meta content="Coderthemes" name="author">
