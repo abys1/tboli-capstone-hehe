ca1800a691ad1233c7a7b83f5401fd09698c7d20
julianne060701 <fernandez.226952@gensan.sti.edu.ph>
2023-07-13 02:17:23 +0800

    shiitteee quizzz

---
+++ admin_true_false.php
@@ -0,0 +1,118 @@
+<div class="row">
+                    <div class="col-sm-3 mb-2 mb-sm-0">
+                        <div class="nav flex-column nav-pills h5" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+   
+                            <a class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile"
+                                aria-selected="false">
+                                <i class="mdi mdi-account-circle d-md-none d-block"></i>
+                                <span class="d-none d-md-block"> <i class="uil-plus-circle" ></i> &nbsp; Create Multiple Choice Quiz</span>
+                            </a>
+           
+                        </div>
+                    </div> <!-- end col-->
+
+
+                    <div class="row" >
+                                    <div class="card-header">
+
+                                        <h3>Create multiple choice</h3>
+
+                                        <div class="card-body" >
+
+                                        <!-- form start -->
+                                    <form action="#">
+
+
+                                        <div class="mb-3 me-5 ms-4">
+                                            <label for="simpleinput" class="form-label">Questions</label>
+                                            <input type="text" id="simpleinput" class="form-control" placeholder="Enter questions">
+                                        </div>
+
+
+                                        <div class="row me-5 ms-2" >
+                                            <!-- Radios--> 
+
+
+                                                <div class="mt-3">
+                                                    <div class="form-check form-radio-info mb-2">
+                                                        <input type="radio" id="customRadio1" name="customRadio" class="form-check-input">
+                                                        <label class="form-check-label" for="customRadio1">A: is correct?</label>
+                                                        <input type="text" id="simpleinput" class="form-control" placeholder="Enter your text"> 
+                                                    </div>
+                                                    <div class="form-check form-radio-info mb-2">
+                                                    <input type="radio" id="customRadio1" name="customRadio" class="form-check-input">
+                                                        <label class="form-check-label" for="customRadio1">B: is correct?</label>
+                                                        <input type="text" id="simpleinput" class="form-control" placeholder="Enter your text"> 
+                                                    </div>
+                                                    <div class="form-check form-radio-info mb-2">
+                                                    <input type="radio" id="customRadio1" name="customRadio" class="form-check-input">
+                                                        <label class="form-check-label" for="customRadio1">C: is correct?</label>
+                                                        <input type="text" id="simpleinput" class="form-control" placeholder="Enter your text"> 
+                                                    </div>
+                                                    <div class="form-check form-radio-info mb-2">
+                                                    <input type="radio" id="customRadio1" name="customRadio" class="form-check-input">
+                                                        <label class="form-check-label" for="customRadio1">D: is correct?</label>
+                                                        <input type="text" id="simpleinput" class="form-control" placeholder="Enter your text"> 
+                                                    </div>
+                                                </div> 
+
+                                        </div>
+
+                                        
+                                            
+
+
+                                 
+
+                                    
+                                    
+                                    
+                                    </div>
+
+                                    <div class="card-footer" >
+                                        <input type="button" class=" btn btn-info" value="Submit">
+                                        <input type="reset" class=" btn btn-info" value="reset">
+                                    </div>
+
+                                        </form>
+                                        <!-- endd form -->
+                                
+                            
+
+                                        
+
+                                        </div>
+                                        <!-- end card-body-->
+
+                                    </div>
+                                    <!--end card header -->
+
+                                    
+
+                                    
+                                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                
+                                <!-- end content here -->
+                            </div>
+                            <!-- <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                                <p class="mb-0">...</p>
+                            </div> -->
+                        </div> <!-- end tab-content-->
+                    </div> <!-- end col-->
+                </div>
+                <!-- end row-->
+
+
