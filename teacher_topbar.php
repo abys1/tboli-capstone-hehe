8d9017659cb05c593d0af9d2376da8f824a72a1d
julianne060701 <fernandez.226952@gensan.sti.edu.ph>
2023-07-18 19:18:55 +0800

    aajs

--- teacher_topbar.php
+++ teacher_topbar.php
@@ -2,10 +2,10 @@
 <div class="navbar-custom">
     <ul class="list-unstyled topbar-menu float-end mb-0">
         <li class="dropdown notification-list d-lg-none">
-            <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button"
+            <!-- <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button"
                 aria-haspopup="false" aria-expanded="false">
                 <i class="dripicons-search noti-icon"></i>
-            </a>
+            </a> -->
             <div class="dropdown-menu dropdown-menu-animated dropdown-lg p-0">
                 <form class="p-3">
                     <input type="text" class="form-control" placeholder="Search ..." aria-label="Recipient's username">
