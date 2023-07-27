fae23c3f9afe97e59c87503362a4ed087bc8bb35
abys1 <varquez.220494@gensan.sti.edu.ph>
2023-07-28 01:36:46 +0900

    TAMA NA NI

--- Learner_InstructionsQuiz.php
+++ Learner_InstructionsQuiz.php
@@ -299,8 +299,22 @@ $user_id = $_SESSION['user_id'];
                                         echo '</a>';
                                         echo '</div>';
                                     } else {
-                                        echo "No records found";
-                                        exit();
+                                        $sql = "SELECT tbl_quiz_options.quiz_options_id, tbl_quiz_options.instructions, tbl_quiz_true_or_false.true_or_false_id
+                                        FROM tbl_quiz_options
+                                        JOIN tbl_quiz_true_or_false ON tbl_quiz_options.quiz_options_id = tbl_quiz_true_or_false.quiz_options_id
+                                        WHERE tbl_quiz_options.quiz_options_id = $quiz_options_id";
+
+                                        $result = mysqli_query($conn, $sql);
+                                        
+                                        if($result && mysqli_num_rows($result) > 0) {
+                                            $row = mysqli_fetch_assoc($result);
+                                            echo '<p>' . $row['instructions'] . '</p>';
+                                            echo '<div class="card-footer text-md-end">';
+                                            echo '<a href="Learner_Quiz.php?quiz_options_id=' . $row['quiz_options_id'] . '">';
+                                            echo '<button type="button" class="btn btn-info">Take the quiz</button>';
+                                            echo '</a>';
+                                            echo '</div>';
+                                        }
                                     }
 
                                 } else {
--- teacher_add_quiz_trueorfalse.php
+++ teacher_add_quiz_trueorfalse.php
@@ -9,45 +9,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $lesson = $_POST['lesson'];
     $maxScore = $_POST['max'];
     $dateStart = $_POST['date_start'];
-    $allowLate = isset($_POST['allow']) ? 1 : 0;
+    $allowLate = isset($_POST['allow']) ? 1 : 0; 
     $grading = $_POST['grading'];
     $due = $_POST['due'];
     $gradingScore = $_POST['grading_score'];
-    $attempts = $_POST['attempts'];
     $instructions = $_POST['instructions'];
-    $questions = $_POST['question'];
-    $correctChoices = $_POST['choices'];
+    $questions = $_POST['questions']; 
+    $correctChoices = $_POST['correct_choice']; 
 
-    $sql = "INSERT INTO tbl_quiz_options (added_by, title, lesson, max_score, date_start, allow_late, grading, due, grading_score, attempts, instructions) VALUES ('$user_id', '$title', '$lesson', '$maxScore', '$dateStart', $allowLate, '$grading', '$due', '$gradingScore', '$attempts', '$instructions')";
+    $sql = "INSERT INTO tbl_quiz_options (added_by, title, lesson, max_score, date_start, allow_late, grading, due, grading_score, instructions) VALUES ('$user_id', '$title', '$lesson', '$maxScore', '$dateStart', $allowLate, '$grading', '$due', '$gradingScore', '$instructions')";
 
     if ($conn->query($sql) === TRUE) {
         $quiz_options_id = mysqli_insert_id($conn);
 
-        // Loop through the questions and choices
+
         for ($i = 0; $i < count($questions); $i++) {
-            $question_text = $questions[$i];
-            $correct_choice = $correctChoices[$i]['correct'];
+            $question = $questions[$i];
+            $correctChoice = $correctChoices[$i]; 
 
-            // Insert the question and choices into tbl_quiz_true_or_false
-            $sql = "INSERT INTO tbl_quiz_true_or_false (quiz_options_id, question, correct_choice) VALUES ($quiz_options_id, '$question_text', '$correct_choice')";
+            $sql = "INSERT INTO tbl_quiz_true_or_false (quiz_options_id, question, correct_choice) VALUES ($quiz_options_id, '$question', '$correctChoice')";
 
-            if ($conn->query($sql) === TRUE) {
-                // Success message or redirect if needed
+            if ($conn->query($sql) !== TRUE) {
+                echo "Error: " . mysqli_error($conn);
+                exit();
             } else {
-                // Error occurred
                 echo "Error: " . mysqli_error($conn);
             }
         }
-
-        // Redirect to a success page or show a success message
-        header("Location: Teacher_index.php?msg=Quiz - true or false added successfully");
+        header("Location: Teacher_index.php?msg=Quiz - True or False added successfully");
         exit();
     } else {
-        // Error occurred
+
         echo "Error: " . mysqli_error($conn);
     }
 
-    // Close the connection
+
     mysqli_close($conn);
 }
 ?>
